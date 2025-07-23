fdeab1b562934c1133c499ae2fcbb60834b14548
aivargab <aivargab@mail.ru>
2025-07-23 17:11:14 +0500

    Mongo URL деплой фикс

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,19 +3,20 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UsersModule } from './users/users.module';
-import { ConfigModule, ConfigService } from '@nestjs/config'
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+      envFilePath: process.env.NODE_ENV === 'production' ? '.env.production' : '.env.local',
     }),
-    MongooseModule.forRootAsync({ 
+    MongooseModule.forRootAsync({
       imports: [ConfigModule],
+      inject: [ConfigService],
       useFactory: async (configService: ConfigService) => ({
         uri: configService.get<string>('MONGO_DB_URL'),
       }),
-      inject: [ConfigService],
     }),
     UsersModule,
   ],
